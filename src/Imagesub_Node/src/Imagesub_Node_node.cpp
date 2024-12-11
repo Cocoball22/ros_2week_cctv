045e943e347d79e7f847bfc2d2d15dbd9a69de67
Cocoball22 <sungtoo412@naver.com>
2024-12-11 14:13:33 +0900

    compressed_image

--- src/Imagesub_Node/src/Imagesub_Node_node.cpp
+++ src/Imagesub_Node/src/Imagesub_Node_node.cpp
@@ -1,44 +1,88 @@
 #include <ros/ros.h>
+// 이미지 전송을 위한 ROS 패키지 헤더
 #include <image_transport/image_transport.h>
+// OpenCV 화면 출력 등을 위한 헤더
 #include <opencv2/highgui/highgui.hpp>
+// ROS 이미지 메시지와 OpenCV 이미지 간의 변환을 위한 헤더
 #include <cv_bridge/cv_bridge.h>
+// ROS 이미지 메시지 타입 헤더
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CompressedImage.h>
 
 class Imagesub_Node{
 private:
     ros::NodeHandle nh;
-    ros::Subscriber image_sub;
+    // 이미지 구독자 객체
+    ros::Subscriber image_sub_;
+    // 이미지 퍼블리셔 객체
+    ros::Publisher image_compressed_pub_;
 
 public:
     Imagesub_Node()
     {
-        image_sub = nh.subscribe("/camera/color/image_raw",10,&Imagesub_Node::Subscriber_image,this);
-
+        // camera/color/image_raw 토픽을 받아 이미지
+        image_sub_ = nh.subscribe("/camera/color/image_raw",10,&Imagesub_Node::imageCb,this);
+        // 압축된 이미지로 전송
+        image_compressed_pub_ = nh.advertise<sensor_msgs::CompressedImage>("/camera/color/image_raw/compressed",10);
 
     }
 
-    void Subscriber_image(const sensor_msgs::ImageConstPtr& msg)
-    {
-        try
-        {
-            cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-            cv::waitKey(30);
-        }
-        catch(const std::exception& e)
-        {
-            ROS_ERROR("Could not convert from '%s' to 'bgr8.", msg->encoding.c_str());
-        }
-    }
+// 이미지를 영상으로 부터 수신하는 서브스크라이버 sensor_mags/image를 통해서 값을
+    // void Subscriber_image(const sensor_msgs::ImageConstPtr& msg)
+    // {
+    //     try
+    //     {
+    //         //cv_bridge를 사용하여 ROS 이미지 메시지를 OpenCV 이미지로 변환
+    //         //bgr8 형식으로 변환하여 처리
+    //         cv::imshow("RGB Image", cv_bridge::toCvShare(msg, "bgr8")->image);
+    //         cv::waitKey(30);
+    //     }
+    //     catch(const std::exception& e)
+    //     {
+    //         // 이미지 변환 실패 시 에러 메시지 출력
+    //         // 현재 인코딩 형식을 포함하여 에러 메시지 표시
+    //         ROS_ERROR("Could not convert from '%s' to 'bgr8.", msg->encoding.c_str());
+    //     }
+    // }
     
+    void imageCb(const sensor_msgs::ImageConstPtr& msg)
+   {
+     cv_bridge::CvImagePtr cv_ptr;
+     try
+     {
+       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+     }
+     catch (cv_bridge::Exception& e)
+     {
+       ROS_ERROR("cv_bridge exception: %s", e.what());
+       return;
+     }
+ 
+     // Draw an example circle on the video stream
+     if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+      cv::circle(cv_ptr->image, cv::Point(250, 250), 10, CV_RGB(255,0,0));
+ 
+     // Update GUI Window
+     cv::imshow("OPENCV_WINDOW", cv_ptr->image);
+     cv::waitKey(3);
+ 
+ 
+     // Output modified video stream
+     image_compressed_pub_.publish(cv_ptr->toImageMsg());
+   }
 };
 
 int main(int argc, char** argv)
 {
-    
+    // ROS 노드 초기화
+    // 노드 이름을 "Imagesub_Node"로 설정
     ros::init(argc, argv, "Imagesub_Node");
     
+    // Imagesub_Node 클래스의 인스턴스 생성
     Imagesub_Node Imagesub_Node_;
 
+    // ROS 메시지 루프 실행
+    // 모든 콜백 함수들이 호출될 수 있도록 대기 
     ros::spin();
 
 }
